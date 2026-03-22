c3926ef14c3e99f665e20f564a6e45309bc530d1
Itay Rosen <itayrosen04@gmail.com>
2026-03-22 19:40:32 +0200

    refactor: restore MainForStudent and Student classes, update Vector class structure

--- Week_1/Presentation/MainForStudent.java
+++ Week_1/Presentation/MainForStudent.java
@@ -1,16 +1,14 @@
 package Week_1.Presentation;
-// public class MainForStudent {
-// 	public static void main(String[] args) {
-// 		Student s = new Student("Alfonso", 101);
-// 		for (int i = 0; i < 10; i++)
-// 			s.addCourse();
-// 		System.out.println(s);
-// 		s.removeCourse();
-// 		System.out.println(s);
-// 	}
-// 	// Alfonso(101) : 10 courses
-// 	// Alfonso(101) : 9 courses
-// }
 
-
-
+public class MainForStudent {
+	public static void main(String[] args) {
+		Student s = new Student("Alfonso", 101);
+		for (int i = 0; i < 10; i++)
+			s.addCourse();
+		System.out.println(s);
+		s.removeCourse();
+		System.out.println(s);
+	}
+	// Alfonso(101) : 10 courses
+	// Alfonso(101) : 9 courses
+}
--- Week_1/Presentation/Vector.java
+++ Week_1/Presentation/Vector.java
@@ -1,35 +1,45 @@
 package Week_1.Presentation;
+
 public class Vector {
 	private double x, y;
 
-	public Vector(double x1, double y1) {
-		// x = x1 < 0 ? 0 : x1;
-		// y = y1 < 0 ? 0 : y1; זה שכפול!
-
-		setX(x1);
-		setY(y1);
+	public Matrix(Vector v1, Vector v2) {
+		vector1[][] = v1
 	}
 
-	public double getX() { return x; }
-	public double getY() { return y; }
-	public void setX(double x1) { x = x1 < 0 ? 0 : x1; }
-	public void setY(double y1) { y = y1 < 0 ? 0 : y1; }
-	
-	public String toString() { return "<" + x + "," + y + ">"; }
-	
-	public double distFrom0() { return Math.sqrt(x * x + y * y); }
+	public void multByConstant(double c) { }
 
-	public double distFromOther(Vector other) {
-		return Math.sqrt(Math.pow(x - other.x, 2) + 
-				Math.pow(y - other.y, 2));
-	}
+	public Matrix add(Matrix other) { }
 
-	public void addToMe(Vector other) {
-		x += other.x;
-		y += other.y;
-	}
 
-	public Vector add(Vector other) {
-		return new Vector(x + other.x, y + other.y);
-	}
+
 }
+
+// public class Vector {
+// 	private double x, y;
+
+// 	public Vector(double x, double y) {
+// 		this.x = x;
+// 		this.y = y;
+// 	}
+// 	public double getX() { return x; }
+// 	public double getY() { return y; }
+	
+// 	public String toString() {
+// 		return "<" + x + "," + y + ">";
+// 	}
+	
+// 	public void multByConstant(double c) {
+// 		x *= c;
+// 		y *= c;
+// 	}
+	
+// 	public void addToMe(Vector other) {
+// 		x += other.x;
+// 		y += other.y;
+// 	}
+
+// 	public Vector add(Vector other) {
+// 		return new Vector(x + other.x, y + other.y);
+// 	}
+// }
